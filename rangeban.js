1b91f7fdb7349c30f3fd0dd0851168d6b68667db
Joshua Taylor <lordralex@gmail.com>
2015-12-10 23:47:15 -0600

    Do slower

--- rangeban.js
+++ rangeban.js
@@ -1,5 +1,12 @@
-ipMask="x.y.z."
-for(i=115; i <= 115; i++) {
-    $("#field-ipAddressToBan").val(ipMask + i);
+ipMask = "x.y.z."
+ban(ipMask, 0, 255);
+
+function ban(mask, i, end) {
+    if (i > end) {
+        return;
+    }
+    $("#field-ipAddressToBan").val(mask + i);
+    console.log('Banning ' + mask + i);
     $.post('/cp/ban-ip', $('form').serialize());
+    setTimeout(ban, 1000, mask, val + 1, end);
 }
