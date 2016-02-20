7468a0980f42b0d4847934fcecf5dc88fa225228
Lord_Ralex <lordralex@gmail.com>
2016-02-20 17:20:13 -0600

    Bold sections if they have unread threads

--- fixes/unreadthread.js
+++ fixes/unreadthread.js
@@ -1,3 +1,7 @@
 $('tr.forum-thread-row-unread').find('a.title').each(function () {
     $(this).css({"font-weight": "bold"});
 });
+
+$('tr.forum-row-not-seen-latest-thread').find('h4.title > a').each(function () {
+    $(this).css({"font-weight": "bold"});
+});
